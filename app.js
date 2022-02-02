9edbf7e1f00e38d284841a7ce92f6fc90b284df8
Amiran Kiladze <amirani.kiladze99@gmail.com>
2022-02-02 15:42:26 +0400

    website project
    This one page website which is responsive and as the width of the window shrinks navbar content disappears  menu button appears

---
+++ app.js
@@ -0,0 +1,63 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar_menu");
+const navLogo = document.querySelector("#navbar_logo");
+
+// Display mobile menu
+
+const mobileMenu = () => {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+}
+
+menu.addEventListener("click", mobileMenu);
+
+// showa active menu when scrolling 
+
+const highlightMenu = () => {
+    const elem = document.querySelector("highlight");
+    const homeMenu = document.querySelector("#home-page");
+    const aboutMenu = document.querySelector("#about-page");
+    const servicesMenu = document.querySelector("#services-page");
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos)
+
+    if (window.innerWidth > 960 && scrollPos < 500) {
+        homeMenu.classList.add("highlight");
+        aboutMenu.classList.remove("highlight");
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add("highlight")
+        homeMenu.classList.remove("highlight");
+        servicesMenu.classList.remove("highlight");
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 2200) {
+        servicesMenu.classList.add("highlight");
+        aboutMenu.classList.remove("highlight");
+        return
+    }    else {
+        aboutMenu.classList.remove("highlight")
+        homeMenu.classList.remove("highlight");
+        servicesMenu.classList.remove("highlight")
+    }
+
+    if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove("highlight")
+    }       
+}
+
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+
+// Close mobile Menu when clicking on a menu item 
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector(".is-active");
+    if (window.innerWidth <= 960 && menuBars) {
+        menu.classList.toggle("is-active");
+        menuLinks.classList.remove("active");
+    }
+};
+
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
